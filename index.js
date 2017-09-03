d48035d5a55311cd07c975e94383a8de4fa9c73e
Thamyres Guedes <thataguedis@gmail.com>
2017-09-02 21:09:12 -0300

    Adicionando o diretorio de fontes para ser lido

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.set('view engine', 'pug')
 
 app.use('/assets/css', expressLess(__dirname + '/src/assets/less', { compress: true }));
 app.use('/assets/img', express.static(__dirname + '/src/assets/img'));
+app.use('/assets/fonts', express.static(__dirname + '/src/assets/fonts'));
 
 app.get('/', function(req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!' })
